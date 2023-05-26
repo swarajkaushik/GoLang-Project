9f5d6961d44069b9c5494f2dc8ccd86c597ca087
Swaraj Kaushik <swarajkaushik@gmail.com>
2023-05-26 10:37:07 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	routes "GoLang-Project/routes"
+	"os"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port = os.Getenv("PORT")
+
+	if port="" {
+		port="8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.Get("/api-1", func(c *gin.Context){
+		c.JSON(200, gin.H{"success": "Access franted for api-1"})
+	})
+
+	router.GET("/api-1", func(c *gin.Context){
+		c.JSON(200, gin.h{"success": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
+}
---
+++ routes/userRouter.go
@@ -0,0 +1,13 @@
+package routes
+
+import(
+	controller "GoLang-Project/controllers"
+	"Golang-Project/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
