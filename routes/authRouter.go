47f12c6348b8f3c76e25479287dad24475310bd9
Swaraj Kaushik <swarajkaushik@gmail.com>
2023-05-28 17:25:31 +0530

    updates

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,11 +1,11 @@
 package routes
 
 import(
-	controller "github.com/swarajkaushik/GoLang-Project/controllers"
+	controllers "github.com/swarajkaushik/GoLang-Project/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func AuthRoutes(incomingRoutes *gin.Engine){
-	incomingRoutes.POST("users/signup", controller.Signup())
-	incomingRoutes.POST("users/login", controller.Login())
+	incomingRoutes.POST("users/signup", controllers.Signup())
+	incomingRoutes.POST("users/login", controllers.Login())
 }
